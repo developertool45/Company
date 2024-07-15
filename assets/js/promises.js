1203a6e00f4ef86df3bdd463eb64ead5776539d7
developertool45 <developertool45@gmail.com>
2024-07-15 17:42:24 +0530

    update

--- assets/js/promises.js
+++ assets/js/promises.js
@@ -95,13 +95,93 @@ function objCopy(username, age, isLogIn) {
     this.username = username;
     this.age = age;
     this.isLogIn = isLogIn;
-
-    return this;
 }
-
+objCopy.prototype.printUsername = function () {
+    console.log(`username of objcopy is ${this.username}`);
+}
+objCopy.prototype.printUserAge = function () {
+     console.log(`UserAge of objcopy is ${this.age}`);
+}
 const u1 = new objCopy("Rajneesh", 27, true);
 const u2 = new objCopy("kumar", 25, false);
 
 
-console.log(u1.username);
-console.log(u2.age);
+//  u1.printUserAge();
+
+
+let objOne = {
+    username: "rajneesh",
+    age: 27,
+    location: "dehradun",
+    naresh: function () {
+        return console.log(`username is ${this.username}`);
+    }
+}
+
+
+class myProgram{
+    constructor(myname, age, day) {
+        this.myname = myname;
+        this.age = age;
+        this.day = day;
+    }
+}
+
+
+function Program(myname, age, day){     
+    this.myname = myname;
+    this.age = age;
+    this.day = day; 
+    
+    return this;
+}
+
+
+let data = new myProgram("Naresh kumar", 25, "monday");
+
+let data1 = new Program("Naresh", 25, "monday");
+let data2 = new Program("Ajay ji", 24, "monday");
+
+// console.log(data1);
+// console.log(data2);
+// console.log(data1.myname);
+// console.log(data2.age);
+
+
+
+
+let naresh = {
+    yourName : "Naresh and Ajay",
+    age: 26, 
+    
+}
+
+let ajayji = {
+    Name : "Ajay",
+    age : 24,
+    village: "kuakhera",
+    tai: "naresh ki mummy",
+    
+}
+
+naresh.__proto__ = ajayji;    
+
+// Object.setPrototypeOf(naresh, ajayji);
+
+// console.log(naresh.Name);
+
+//===============this and call ========================//
+
+function Setusername(username) {
+    this.username = username;
+}
+
+function createUser(username, email, password) {
+    Setusername.call(this, username)
+    this.email = email;
+    this.password = password;
+}
+
+
+const usr1 = new createUser("Rajneesh kumar", "email@gmail.com", "Raj123");
+console.log(usr1);
